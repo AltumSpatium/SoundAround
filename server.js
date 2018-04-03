2f474c5a1fd1be0581207830d5ebcfd52900ef8c
Alex Martynov <sinner.in.me11@gmail.com>
2018-04-03 16:47:41 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const Express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json({limit: '50mb'});
+const urlencodedParser = bodyParser.urlencoded({limit: '50mb', extended: true});
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://Spital:Topaz123_@ds131989.mlab.com:31989/sound-around-db', { useMongoClient: true });
+
+const app = new Express();
+const port = 3001;
+
+app.use(Express.static(__dirname));
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/build/index.html');
+});
+
+app.listen(port, error => {
+    if (error) console.log(error);
+    else console.info(`==> Listening on port ${port}`);
+});
